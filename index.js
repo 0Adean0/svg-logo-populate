f32afe52051420fbfa480b003f870c31a994e7d9
MuheCa <119535228+0Adean0@users.noreply.github.com>
2023-04-18 09:38:57 -0400

    completed first half of index, now working on then response and the writing of the svg

--- index.js
+++ index.js
@@ -5,3 +5,32 @@ const shapes = require("./lib/shapes")
 
 
 inquirer.logPrompt("inputMaximumLengthPrompt", inputMaximumLength)
+
+inquirer.prompt([{
+type:"inputMaximumLengthPrompt",
+name:"text",
+message:"Type your text that will appear on your logo. Please type no more than 3 characters",
+maxLength: 3,
+},
+{ 
+ type: "input",
+ name: "textColor",
+ message: "Type yourdesired text color using a color keyword or its hexadecimal values if known"
+},
+{
+    type:"list",
+    name: "shape",
+    message: "What shape would you like to have presented from the choices provided?",
+    choices: ["Circle","Triangle","Rectangle"],
+},
+{
+    type: "input",
+    name: "shapeColor",
+    message: "Type your desired shape color using a color keyword or its hexidecimal values if known"
+}
+
+}
+
+
+
+}])
